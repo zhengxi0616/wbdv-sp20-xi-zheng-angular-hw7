8bb2a4fd288972fde7635dd7be1b461ed6ee4ac0
wuyushuwys <wuyushuwys@outlook.com>
2020-03-28 22:00:41 -0400

    assignment7 create lesson service

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -8,8 +8,4 @@ export class CourseServiceClient {
   findCourseById = (courseId) =>
     fetch(`http://wbdv-generic-server.herokuapp.com/api/001493040/courses/${courseId}`)
       .then(response => response.json())
-
-  findLessonsForModule = (moduleId) =>
-    fetch(`wbdv-generic-server.herokuapp.com/api/001493040/courses/${courseId}/modules`)
-      .then(response => response.json())
 }

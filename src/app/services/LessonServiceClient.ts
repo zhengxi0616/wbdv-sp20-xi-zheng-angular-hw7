acb69d40d588e7cf78e07f90846a1c18068e4c67
wuyushuwys <wuyushuwys@outlook.com>
2020-03-29 22:04:40 -0400

    Assignment 7 combine module list in the course viewer

--- src/app/services/LessonServiceClient.ts
+++ src/app/services/LessonServiceClient.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class LessonServiceClient{
   findLessonsForModule = (moduleId) =>
-    fetch(`wbdv-generic-server.herokuapp.com/api/001493040/modules/${moduleId}/lessons`)
+    fetch(`http://wbdv-generic-server.herokuapp.com/api/001493040/modules/${moduleId}/lessons`)
       .then(response => response.json())
 }
